3ab8f5736eefc280c548856dac2f533afda0d7b2
AA64 <coldcore@mail.ru>
2016-05-18 04:36:54 +0300

    remaking header

--- header.php
+++ header.php
@@ -14,33 +14,38 @@
 		<nav id="top-header">
 			<div id="header-center">
 <?php
-	if (isset($_SESSION['username']) && isset($_SESSION['user_id']) 
-		&& isset($_SESSION['is_student'])) {
+	if (isset($_SESSION['username']) && isset($_SESSION['first_name']) 
+		&& isset($_SESSION['second_name']) && isset($_SESSION['is_student'])) {
 		//well, that is a special and the complex one case
+		$username_h = mb_substr($_SESSION['username'], 0, stripos($_SESSION['username'], '@')+1);
+		echo "\t\t\t\t<div id=\"menu-right-personified\">";
+		echo "\t\t\t\t\t<a href=\"\"><span id=\"mail_part\">" . $username_h . " </span><span id=\"name_part\">" . $_SESSION['first_name'] . " " . $_SESSION['second_name'] . "</span></a>";
+		
+		if ($_SESSION['is_student'] == 1) {
+
+		}
+		else {
+
+		}
 	}
 	else {
 ?>
 				<div id="menu-right">
-					<div id="menu-right-login">
-						<a class="menu-right-href" href="login.html">
-							<p>Вход</p>
-						</a>
-					</div>
+					<div id="menu-right-dummy"></div>
 					<div id="menu-right-signup">
-						<a class="menu-right-href" href="#">
-							<p>Регистрация</p>
+						<a class="menu-right-href" href="signin.php">
+							<p>Вход / Регистрация</p>
 						</a>
 					</div>
 				</div>
 				<div id="menu-left">
 					<div id="menu-left-logo">
-						<a href="index.html">
+						<a href="index.php">
 							<span id="logo-left">ОНЛАЙН</span>
 							<span id="logo-mid">/</span>
 							<span id="logo-right">ШКОЛА</span>
 						</a>
 					</div>
-					<div id="menu-left-additional"></div>
 				</div>
 				<div id="menu-dummy"></div>
 			</div>
--- test.php
+++ test.php
@@ -1,13 +1,5 @@
 <?php
-	$page_title = 'Регистрация';
-	$page_css = 'signup.css';
-	$page_js = 'signup.js';
-	require_once('header.php');
-?>
-			<div id="first-background">
-			<div id="reg-box">
-				<p>Регистрация</p>
-				<div id="inner-reg-box">	
-<?php
-	require_once('student_form.php');
+	$username = 'coldcore@mail.com';
+	$username = mb_substr($username, 0, stripos($username, '@')+1);
+	echo $username;
 ?>
