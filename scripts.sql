bf7beeafc7757ca45016429f896ef5c5fe74ddd1
AA64 <coldcore@mail.ru>
2016-06-01 15:22:07 +0300

    student index ready

--- scripts.sql
+++ scripts.sql
@@ -1,8 +1,77 @@
-CREATE TABLE users (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, login_mail VARCHAR(30) NOT NULL, password VARCHAR(40) NOT NULL, is_student BOOL)
+CREATE TABLE users (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, login_mail VARCHAR(30) 
+	NOT NULL, password VARCHAR(40) NOT NULL, is_student BOOL);
 
-CREATE TABLE parents (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, user_id INT NOT NULL, first_name VARCHAR(30) NOT NULL, 
-	second_name VARCHAR(30) NOT NULL, patronymic VARCHAR(20), FOREIGN KEY(user_id) REFERENCES users(id))
+CREATE TABLE parents (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, user_id INT NOT NULL, 
+	first_name VARCHAR(30) NOT NULL, second_name VARCHAR(30) NOT NULL, patronymic VARCHAR(20), 
+	FOREIGN KEY(user_id) REFERENCES users(id));
 
-CREATE TABLE students (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, user_id INT NOT NULL, first_name VARCHAR(30) NOT NULL, 
-	second_name VARCHAR(30) NOT NULL, patronymic VARCHAR(20), birth_date DATE NOT NULL, location VARCHAR(30) NOT NULL, 
-	school VARCHAR(30) NOT NULL, photo VARCHAR(30) NOT NULL, parent_one INT, parent_two INT, FOREIGN KEY(user_id) REFERENCES users(id));
+CREATE TABLE students (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, user_id INT NOT NULL, 
+	first_name VARCHAR(30) NOT NULL, second_name VARCHAR(30) NOT NULL, patronymic VARCHAR(20), 
+	birth_date DATE NOT NULL, location VARCHAR(30) NOT NULL, school VARCHAR(30) NOT NULL, 
+	photo VARCHAR(30) NOT NULL, parent_one INT, parent_two INT, FOREIGN KEY(user_id) 
+	REFERENCES users(id));
+
+CREATE TABLE subjects (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, subject_name VARCHAR(20) 
+	NOT NULL)
+
+CREATE TABLE themes (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, subject_id INT NOT NULL, 
+	theme_name VARCHAR(30) NOT NULL, order_number INT NOT NULL, link VARCHAR(20));
+
+CREATE TABLE theme_progress (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, student_id INT 
+	NOT NULL, theme_id INT NOT NULL, theme_status VARCHAR(1));
+#s - started   e - ended   n - new
+
+INSERT INTO subjects (subject_name) VALUES ('Алгебра');
+INSERT INTO subjects (subject_name) VALUES ('Геометрия');
+INSERT INTO subjects (subject_name) VALUES ('Физика');
+INSERT INTO subjects (subject_name) VALUES ('Химия');
+INSERT INTO subjects (subject_name) VALUES ('Биология');
+
+INSERT INTO theme_progress (student_id, theme_id, theme_status) VALUES (1, 16, 'e');
+INSERT INTO theme_progress (student_id, theme_id, theme_status) VALUES (1, 17, 'e');
+INSERT INTO theme_progress (student_id, theme_id, theme_status) VALUES (1, 18, 'e');
+INSERT INTO theme_progress (student_id, theme_id, theme_status) VALUES (1, 19, 'e');
+INSERT INTO theme_progress (student_id, theme_id, theme_status) VALUES (1, 20, 's');
+INSERT INTO theme_progress (student_id, theme_id, theme_status) VALUES (1, 21, 'n');
+INSERT INTO theme_progress (student_id, theme_id, theme_status) VALUES (1, 22, 'n');
+INSERT INTO theme_progress (student_id, theme_id, theme_status) VALUES (1, 23, 'n');
+
+INSERT INTO themes (subject_id, theme_name, order_number, link) VALUES (1, 'Рациональные дроби', 1, 'blank');
+INSERT INTO themes (subject_id, theme_name, order_number, link) VALUES (1, 'Произведение и частное дробей', 2, 'blank');
+INSERT INTO themes (subject_id, theme_name, order_number, link) VALUES (1, 'Обратная пропорциональноcть', 3, 'blank');
+INSERT INTO themes (subject_id, theme_name, order_number, link) VALUES (1, 'Действительные числа', 4, 'blank');
+INSERT INTO themes (subject_id, theme_name, order_number, link) VALUES (1, 'Квадратный корень', 5, 'blank');
+INSERT INTO themes (subject_id, theme_name, order_number, link) VALUES (1, 'Квадратное уравнение', 6, 'blank');
+INSERT INTO themes (subject_id, theme_name, order_number, link) VALUES (1, 'Числовые неравенства', 7, 'blank');
+INSERT INTO themes (subject_id, theme_name, order_number, link) VALUES (2, 'Повороты', 1, 'blank');
+INSERT INTO themes (subject_id, theme_name, order_number, link) VALUES (2, 'Тригонометрические функции', 2, 'blank');
+INSERT INTO themes (subject_id, theme_name, order_number, link) VALUES (2, 'Вписанные и описанные фигуры', 3, 'blank');
+INSERT INTO themes (subject_id, theme_name, order_number, link) VALUES (2, 'Правильные многоугольники', 4, 'blank');
+INSERT INTO themes (subject_id, theme_name, order_number, link) VALUES (2, 'Круг и число п', 5, 'blank');
+INSERT INTO themes (subject_id, theme_name, order_number, link) VALUES (2, 'Тригонометрические функции', 6, 'blank');
+INSERT INTO themes (subject_id, theme_name, order_number, link) VALUES (2, 'Площадь поверхностей', 7, 'blank');
+INSERT INTO themes (subject_id, theme_name, order_number, link) VALUES (2, 'Объемы тел', 8, 'blank');
+INSERT INTO themes (subject_id, theme_name, order_number, link) VALUES (3, 'Электрический заряд', 1, 'blank');
+INSERT INTO themes (subject_id, theme_name, order_number, link) VALUES (3, 'Закон сохранения заряда', 2, 'blank');
+INSERT INTO themes (subject_id, theme_name, order_number, link) VALUES (3, 'Взаимодействие зарядов', 3, 'blank');
+INSERT INTO themes (subject_id, theme_name, order_number, link) VALUES (3, 'Электрическое поле', 4, 'blank');
+INSERT INTO themes (subject_id, theme_name, order_number, link) VALUES (3, 'Закон Ома', 5, 'om_rule');
+INSERT INTO themes (subject_id, theme_name, order_number, link) VALUES (3, 'Последовательное соединение', 6, 'blank');
+INSERT INTO themes (subject_id, theme_name, order_number, link) VALUES (3, 'Параллельное соединение', 7, 'blank');
+INSERT INTO themes (subject_id, theme_name, order_number, link) VALUES (3, 'Работа и мощность тока', 8, 'blank');
+INSERT INTO themes (subject_id, theme_name, order_number, link) VALUES (4, 'Атомы и молекулы', 1, 'blank');
+INSERT INTO themes (subject_id, theme_name, order_number, link) VALUES (4, 'Понятие вещества', 2, 'blank');
+INSERT INTO themes (subject_id, theme_name, order_number, link) VALUES (4, 'Масса веществ', 3, 'blank');
+INSERT INTO themes (subject_id, theme_name, order_number, link) VALUES (4, 'Система химических элементов', 4, 'blank');
+INSERT INTO themes (subject_id, theme_name, order_number, link) VALUES (4, 'Валентность элементов', 5, 'blank');
+INSERT INTO themes (subject_id, theme_name, order_number, link) VALUES (5, 'Химические реакции', 6, 'blank');
+INSERT INTO themes (subject_id, theme_name, order_number, link) VALUES (4, 'Чистые вещества и смеси', 7, 'blank');
+INSERT INTO themes (subject_id, theme_name, order_number, link) VALUES (4, 'Газы', 8, 'blank');
+INSERT INTO themes (subject_id, theme_name, order_number, link) VALUES (5, 'Организм человека', 1, 'blank');
+INSERT INTO themes (subject_id, theme_name, order_number, link) VALUES (5, 'Опорно-двигательный аппарат', 2, 'blank');
+INSERT INTO themes (subject_id, theme_name, order_number, link) VALUES (5, 'Внутренняя среда организма', 3, 'blank');
+INSERT INTO themes (subject_id, theme_name, order_number, link) VALUES (5, 'Кровообращение', 4, 'blank');
+INSERT INTO themes (subject_id, theme_name, order_number, link) VALUES (5, 'Дыхание', 5, 'blank');
+INSERT INTO themes (subject_id, theme_name, order_number, link) VALUES (5, 'Питание', 6, 'blank');
+INSERT INTO themes (subject_id, theme_name, order_number, link) VALUES (5, 'Обмен веществ', 7, 'blank');
+INSERT INTO themes (subject_id, theme_name, order_number, link) VALUES (5, 'Покровы тела человека', 8, 'blank');
--- stud_subj.php
+++ stud_subj.php
@@ -1,39 +1,99 @@
 <?php
-	/*$dbc = mysqli_connect('localhost', 'headfirst', 'headfirst', 'temp_learning') 
+	$dbc = mysqli_connect('localhost', 'headfirst', 'headfirst', 'temp_learning') 
 		or die('error connecting with database :(');
 	mysqli_query($dbc, "SET NAMES 'utf8'");
 
-	function coagulator() {
-		$query = "SELECT "
-	}*/
-?>		
-		<div id="first_background">
-			<p id="p-choice">Выбор предметов и тем</p>
-			<div id="subject-container">
-				<div id="inner-subject-container">
-					<p class="p-subject" id="algebra">Алгебра</p>
-					<div class="divider"></div>
-					<p class="p-subject" id="geometry">Геометрия</p>
-					<div class="divider"></div>
-					<p class="p-subject" id="physics">Физика</p>
-					<div class="divider"></div>
-					<p class="p-subject" id="chems">Химия</p>
-					<div class="divider"></div>
-					<p class="p-subject" id="biology">Биология</p>
-					<div class="divider"></div>
-				</div>
-				<div class="inner-theme-container">
-					<div class="para"><a href="om_rule.php">Paragraph</a></div>
-					<div class="para"><a href="#">Paragraph</a></div>
-					<div class="para"><a href="#">Paragraph</a></div>
-					<div class="para"><a href="#">Paragraph</a></div>
-					<div class="para"><a href="#">Paragraph</a></div>
-					<div class="para"><a href="#">Paragraph</a></div>
-					<div class="para"><a href="#">Paragraph</a></div>
-					<div class="para"><a href="#">Paragraph</a></div>
-					<div class="para"><a href="#">Paragraph</a></div>
-				</div>
-			</div>
+	function coagulator($keyword, $dbc) {
+		//getting student id
+		$query = "SELECT id FROM students WHERE user_id = '" . $_SESSION['user_id'] . "'";
+		$data = mysqli_query($dbc, $query);
+		$row = mysqli_fetch_array($data);
+		$student_id = $row['id'];
 
+		//getting subject id
+		$query = "SELECT id FROM subjects WHERE subject_name = '$keyword'";
+		$data = mysqli_query($dbc, $query);
+		$row = mysqli_fetch_array($data);
+		$subject_id = $row['id'];
+		$query = "SELECT * FROM themes WHERE subject_id = '$subject_id' ORDER BY order_number ASC";
+		$data = mysqli_query($dbc, $query);
+
+		while ($row = mysqli_fetch_array($data)) {
+			$theme_name = $row['theme_name'];
+			$theme_id = $row['id'];
+			$link = $row['link'];
+			$order_number = $row['order_number'];
+			
+			$proquery = "SELECT theme_status FROM theme_progress WHERE theme_id = '$theme_id' AND 
+				student_id = '$student_id'";
+			$prodata = mysqli_query($dbc, $proquery);
+			$prorow = mysqli_fetch_array($prodata);
+			
+			if ($prorow['theme_status'] == 'n' || $prorow['theme_status'] == NULL){
+				echo "<div class=\"paragraph\"><a class=\"new-para\" href=\"" . $link . ".php\">" . 
+					$order_number . "." . $theme_name . "</a></div>";
+			} elseif ($prorow['theme_status'] == 'e') {
+				echo "<div class=\"paragraph\"><a class=\"ended-para\" href=\"" . $link . ".php\">" . 
+					$order_number . "." . $theme_name . "</a></div>";
+			} elseif ($prorow['theme_status'] == 's') {
+				echo "<div class=\"paragraph\"><a class=\"start-para\" href=\"" . $link . ".php\">" . 
+					$order_number . "." . $theme_name . "</a></div>";
+			}
+		}
+	}
+?>		
+		<p id="p-choice">Выбор предметов и тем</p>
+		<div id="student-first-background">
+			<div id="subject-container">
+				<div class="subject-row">
+					<div class="inner-subject-container" id="algebra">
+						<p class="p-subject">Алгебра</p>
+					</div>
+					<div class="inner-theme-container" id="algebra-links">
+<?php
+	coagulator('Алгебра', $dbc);
+?>
+					</div>
+				</div>
+				<div class="subject-row">
+					<div class="inner-subject-container" id="geometry">
+						<p class="p-subject">Геометрия</p>
+					</div>
+					<div class="inner-theme-container" id="geometry-links">
+<?php
+	coagulator('Геометрия', $dbc);
+?>
+					</div>
+				</div>
+				<div class="subject-row">
+					<div class="inner-subject-container" id="physics">
+						<p class="p-subject">Физика</p>
+					</div>
+					<div class="inner-theme-container" id="physics-links">
+<?php
+	coagulator('Физика', $dbc);
+?>
+					</div>
+				</div>
+				<div class="subject-row">
+					<div class="inner-subject-container" id="chems">
+						<p class="p-subject">Химия</p>
+					</div>
+					<div class="inner-theme-container" id="chems-links">
+<?php
+	coagulator('Химия', $dbc);
+?>
+					</div>
+				</div>
+				<div class="subject-row">
+					<div class="inner-subject-container" id="biology">
+						<p class="p-subject">Биология</p>
+					</div>
+					<div class="inner-theme-container" id="biology-links">
+<?php
+	coagulator('Биология', $dbc);
+?>
+					</div>
+				</div>
 			</div>
 		</div>
